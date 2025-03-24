77e271bfd64d704044f1ee2c59a13d55049a927e
David Kovacs <mrdkvcs@gmail.com>
2025-03-24 22:04:20 +0100

    small change to problem text

--- app/page.tsx
+++ app/page.tsx
@@ -136,7 +136,7 @@ export default function Home() {
         <div className="container">
           <div className="text-center mb-16">
             <h2 className="text-3xl md:text-4xl font-bold text-[#FFFBFC] mb-4">
-              A Problémák, Amiket Megoldunk
+              Válasz az e-mobilitási problémáira:
             </h2>
             <p className="text-[#D9E2E9] max-w-2xl mx-auto">
               A jelenlegi elektromos jármű töltési infrastruktúra Magyarországon
